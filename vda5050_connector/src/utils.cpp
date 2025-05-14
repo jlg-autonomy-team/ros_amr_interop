5a413ce3c9a4421882faace376331787f0f6328e
Guido F. Ritelli <gritelli@jlg.com>
2025-05-13 23:33:49 -0400

    fix: resolve build warnings S587-933

--- vda5050_connector/src/utils.cpp
+++ vda5050_connector/src/utils.cpp
@@ -43,7 +43,7 @@ namespace utils
 bool read_bool_parameter(
   rclcpp::Node * node, const std::string & param_name, const bool default_val)
 {
-  bool temp;
+  bool temp{false};
   node->declare_parameter<bool>(param_name, default_val);
   node->get_parameter(param_name, temp);
   return temp;

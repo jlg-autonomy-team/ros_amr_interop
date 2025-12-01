219fb40ada136b42083a621fa959ef7338ed0492
Guido F. Ritelli <gritelli@jlg.com>
2025-12-01 11:57:27 -0500

    chore: comment jlg change S587-1722

--- vda5050_connector/vda5050_connector_py/mqtt_bridge.py
+++ vda5050_connector/vda5050_connector_py/mqtt_bridge.py
@@ -146,7 +146,9 @@ def generate_vda_order_msg(order):
 
         if "trajectory" in edge:
             edge["trajectory"] = VDATrajectory(
+                # JLG_CHANGES_START
                 degree=int(edge["trajectory"]["degree"]),
+                # JLG_CHANGES_END
                 knot_vector=edge["trajectory"]["knot_vector"],
                 control_points=[
                     VDAControlPoint(
